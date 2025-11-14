7cb46a0560053ba1cb73b84211afb2837138a2b6
CreedTrue <34081464+CreedTrue@users.noreply.github.com>
2025-11-13 20:55:22 -0700

    Health below 0
    I need to add the end game loop. So an end menu with scores and a restart option.
    
    A leader board would also be nice.

--- player_body.gd
+++ player_body.gd
@@ -19,8 +19,14 @@ func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("enemies"):
 		print("Hit by enemy wave")
 		enemy_hit(area.damage)
+	
+	area.queue_free()
 
 
 func enemy_hit(damage):
 	player_health -= damage
+	if player_health < 0:
+		player_health = 0
+		#Player dead send end game signal
+	
 	health_label.set_text(str(ceil(int(player_health))))
