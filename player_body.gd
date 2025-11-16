ed98689942cb9c3a37f7e3805a108757debaa82d
CreedTrue <34081464+CreedTrue@users.noreply.github.com>
2025-11-15 20:27:31 -0700

    Station art and spin
    Station body made and spinning

--- player_body.gd
+++ player_body.gd
@@ -1,18 +1,22 @@
 extends Area2D
 
-var player_health = 100
+var player_health = 500
 var max_health = 1000
 var base_recovery_rate = 1
 @onready var health_label = $HealthLabel
+@onready var sprite_body = $Sprite2D
+var station_spin_rate = 0.5
 signal game_over
 
 func _ready() -> void:
 	health_label.set_text(str(player_health))
 
 func _process(delta: float) -> void:
+	sprite_body.rotate(station_spin_rate * delta)
 	if (player_health < max_health):
 		player_health += (base_recovery_rate * delta)
 		health_label.set_text(str(ceil(int(player_health))))
+	
 		
 
 func _on_area_entered(area: Area2D) -> void:
