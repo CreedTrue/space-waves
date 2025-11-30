677b46bd84fcd1b90a49f3b974026730e5409c9c
CreedTrue <34081464+CreedTrue@users.noreply.github.com>
2025-11-29 20:45:09 -0700

    Credits Page

--- credits.gd
+++ credits.gd
@@ -8,3 +8,11 @@ func _on_back_button_pressed() -> void:
 
 func _on_back_button_button_down() -> void:
 	$"/root/AudioMangerScene".play_audio_omni("button_chip") # Replace with function body.
+
+
+func _on_button_pressed() -> void:
+	OS.shell_open("https://www.douglovelock.com/") # Replace with function body.
+
+
+func _on_button_2_pressed() -> void:
+	OS.shell_open("https://tiny-crystal.itch.io/") # Replace with function body.
